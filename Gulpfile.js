f1391f199f12bae32245e3e22f6e23bebaee19b2
schutzer <erichschutz@gmail.com>
2015-12-31 14:06:05 -0500

    gulp file clean up

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,9 +6,7 @@ var gulp   = require('gulp'),
     preprocess = require('gulp-preprocess'),
     open = require('gulp-open');
 
-gulp.task('dev', ['minify-css', 'minify-html', 'minify-javascript']);
-
-gulp.task('prod', ['set-prod-node-env','html','img','minify-css', 'minify-javascript', 'open']);
+gulp.task('prod', ['html','img','minify-css', 'minify-javascript', 'open']);
 
 gulp.task('minify-css', function() {
     return gulp.src('src/css/*.css')
@@ -16,29 +14,10 @@ gulp.task('minify-css', function() {
         .pipe(gulp.dest('dist/css'));
 });
 
-//gulp.task('minify-html', function() {
-//    var opts = {
-//        conditionals: true,
-//        spare:true
-//    };
-//
-//    return gulp.src('index.html')
-//        .pipe(minifyHTML(opts))
-//        .pipe(gulp.dest('index.html'));
-//});
-
 gulp.task('minify-javascript', function(){
     gulp.src('src/js/*.js')
         .pipe(minifyJavaScript())
-        .pipe(gulp.dest('./dist/js'))
-});
-
-gulp.task('set-dev-node-env', function() {
-    return process.env.NODE_ENV = 'development';
-});
-
-gulp.task('set-prod-node-env', function() {
-    return process.env.NODE_ENV = 'production';
+        .pipe(gulp.dest('dist/js'))
 });
 
 gulp.task('open', function(){
@@ -48,7 +27,7 @@ gulp.task('open', function(){
 
 gulp.task('img', function(){
    gulp.src('src/img/*')
-       .pipe(gulp.dest('./dist/img'));
+       .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('html', function() {
@@ -59,5 +38,5 @@ gulp.task('html', function() {
     gulp.src('src/*.html')
         .pipe(preprocess({context: { NODE_ENV: 'production', DEBUG: true}})) //To set environment variables in-line
         .pipe(minifyHTML(opts))
-        .pipe(gulp.dest('./dist/'))
+        .pipe(gulp.dest('dist/'))
 });
